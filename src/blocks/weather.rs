bf9d7d6b290720c974a5874c3fff80af6f2544d3
nktauserum <auserum@proton.me>
2025-11-02 13:30:12 +0300

    fix: openweathermap, pls stop changing your api

--- src/blocks/weather.rs
+++ src/blocks/weather.rs
@@ -70,8 +70,6 @@ impl Block for WeatherBlock {
 // API response implementations: https://github.com/jt-rose/openweather_sdk
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Sys {
-    #[serde(alias = "type")]
-    pub r_type: u8,
     pub country: String,
     pub sunrise: usize,
     pub sunset: usize,
